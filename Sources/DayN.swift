271cebb1fc82d9e3c3f896624667f67504b3a137
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2024-12-08 11:18:09 +0800

    Update DayN

--- Sources/DayN.swift
+++ Sources/DayN.swift
@@ -1,23 +1,18 @@
 import Foundation
 
 struct DayN: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
-  let data: String
 
   init(data: String) {
-    self.data = data
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    var output = 0
-    return output
+    return 0
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    var output = 0
-    return output
+    return 0
   }
 
 }
