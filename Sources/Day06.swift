8004329eb36838186b9dfe9b9992c53abc618b79
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2024-12-09 22:26:28 +0800

    Day06

---
+++ Sources/Day06.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+final class Day06: AdventDay {
+
+  init(data: String) {
+  }
+
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Any {
+    return 0
+  }
+
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    return 0
+  }
+
+}
